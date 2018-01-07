2611611e721dc4faf3dfe05484cd24f941700eaa
ZAUB1 <aubin.luneau@icloud.com>
2018-01-07 20:01:20 +0100

    Update sql.sql
    Just removed the specified ID to remove the duplicate ID error

--- sql.sql
+++ sql.sql
@@ -1,11 +1,11 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-(39, 'raisin', 'raisin', -1, 0, 1),
-(40, 'vinblanc', 'vin blanc', -1, 0, 1),
-(41, 'vinrouge', 'vin rouge', -1, 0, 1),
-(42, 'grandcru', 'Grand cru', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('raisin', 'raisin', -1, 0, 1),
+('vinblanc', 'vin blanc', -1, 0, 1),
+('vinrouge', 'vin rouge', -1, 0, 1),
+('grandcru', 'Grand cru', -1, 0, 1);
 
-INSERT INTO `jobs` (`id`, `name`, `label`, `whitelisted`) VALUES
-(21, 'vignerons', 'Vignerons', 0);
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('vignerons', 'Vignerons', 0);
 
-INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-(54, 'vignerons', 0, 'interim', 'Intérimaire', 50, '', '');
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('vignerons', 0, 'interim', 'Intérimaire', 50, '', '');
